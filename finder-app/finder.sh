2b1428502a65cd00ced038bab6b05c991801a7b5
TejaswiniLakshminarayan <tela4318@colorado.edu>
2022-08-29 13:02:03 -0600

    addition of comments

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+# filesdir represents the path to a directory on the filesystem
+# searchstr represents a text string which will be searched within these files
 filesdir=$1
 searchstr=$2 
 
+# Check if number of arguments is equal to 2
 if [ "$#" != "2" ];
 then
 	echo "Two arguments needs to be passed"
 	exit 1
 else
+	# Check if filesdir represent a valid directory
 	if [ ! -d "$1" ];
 	then
 		echo "$filesdir does not represent a directory on a file system"
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+# writefile represents full path to a file on the filesystem
+# writestr represents the text string which will be written within this file
 writefile=$1
 writestr=$2 
 
+# check if any arguments were missing
 if [ "$#" != "2" ];
 then
 	echo "Two arguments needs to be passed"
 	exit 1
 else
+	# check if the directory exits
 	if [ ! -d "$1" ];
 	then
+		# creating the path
 		mkdir -p "$(dirname .$1)" && touch ".$1" && echo "$2" > ".$1"
 		
 	else
